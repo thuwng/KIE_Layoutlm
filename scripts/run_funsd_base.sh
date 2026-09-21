78710c7f601a5eff15f3e6d9c318c25325288a13
thuwng <122499901+thuwng@users.noreply.github.com>
2026-09-21 16:50:26 +0700

    Update run_funsd_base.sh

--- scripts/run_funsd_base.sh
+++ scripts/run_funsd_base.sh
@@ -32,7 +32,7 @@ do
     rm -rf "$OUT"
 
     # GỌI PYTHON TỪ CONDA ENV THAY VÌ PYTHON HỆ THỐNG
-    !/kaggle/working/miniconda/envs/layoutlmv3/bin/python -m torch.distributed.run \
+    $PYTHON_CMD -m torch.distributed.launch \
       --nproc_per_node=2 \
       --dataset_name funsd \
       --do_train \
